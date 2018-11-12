d8d4426fdfe339c7626d4f2dc0e4e9c76dc20245
Juliano Levier-Gomes <jl0128@westminstercollege.edu>
2018-11-11 19:46:41 -0700

    added comments to fulfill second commit requirement

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-
+#Checks to see if rel directory exists
 if [ ! -d /var/www/html/A ]
 then
 	mkdir /var/www/html/A
 fi
-
+#Makes the html that will be linked to
 echo "<!DOCTYPE html>" > /var/www/html/A/A.html
 echo "<html>" >> /var/www/html/A/A.html
 echo "<body>" >> /var/www/html/A/A.html
@@ -11,7 +11,7 @@ echo "<h1>Welcome to A<h2>" >> /var/www/html/A/A.html
 
 
 
-
+#Makes index html
 echo "<!DOCTYPE html>" > /var/www/html/index.html
 echo "<html>" >> /var/www/html/index.html
 echo "<body>" >> /var/www/html/index.html
